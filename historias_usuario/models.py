9f99a30926218910ca1977278b16df44d71b2364
Manuel Rene Pauls Toews <contact@renepauls.com>
2022-08-30 10:12:59 -0400

    Comentar historias de usuario y agregar posibilidad de archivos anexos

--- historias_usuario/models.py
+++ historias_usuario/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from proyectos import Proyecto, Sprint
+from usuarios.models import Usuario
 
 
 class EtapaHistoriaUsuario(models.Model):
+    """
+    Etapas de historias de usuario, cada una se muestra como una columna en el tablero de la historia de usuario correspondiente.
+    """
     nombre = models.CharField(max_length=255, unique=True)
     descripcion = models.TextField(blank=True, null=True)
     proyecto = models.ForeignKey(Proyecto, related_name='etapasHistoriaUsuario')
@@ -12,6 +16,10 @@ class EtapaHistoriaUsuario(models.Model):
 
 
 class TipoHistoriaUsusario(models.Model):
+    """
+    Un tipo de historia de usuario. Cada tipo esta relacionado con un proyecto.
+    Se pueden importar en otros proyectos.
+    """
     nombre = models.CharField(max_length=255, unique=True)
     descripcion = models.TextField(blank=True, null=True)
     proyecto = models.ForeignKey(Proyecto, related_name='tiposHistoriaUsuario')
@@ -21,24 +29,47 @@ class TipoHistoriaUsusario(models.Model):
         return self.nombre
 
 
+class ArchivoAnexo(models.Model):
+    """
+    Archivos anexos a una historia de usuario.
+    El nombre del archivo en el servidor es la comcatenacion del nombe y el id del archivo (nombre_id).
+    """
+    nombre = models.CharField(max_length=255, unique=True)
+    archivo = models.FileField(upload_to='archivosAnexo')
+    fecha_subida = models.DateTimeField(auto_now_add=True)
+    subido_por = models.ForeignKey(Usuario, related_name='archivos')
+
+    def __str__(self):
+        return f'{self.nombre}_{self.id}'
+
+
 class HistoriaUsuario(models.Model):
+    """
+    Cada historia de usuario representa un trabajo a realizar.
+    Debe tener UserPoints (up) y BuisnessValue (bv). Se guarda un historial completo.
+    """
     nombre = models.CharField(max_length=255, unique=True)
     descripcion = models.TextField(blank=True, null=True)
     fecha_creacion = models.DateTimeField(auto_now_add=True)
     fecha_modificacion = models.DateTimeField(auto_now=True)
     sprint = models.ForeignKey(Sprint, related_name='historias')
-    estado = models.CharField(max_length=255, blank=True, null=True)
+    etapa = models.ForeignKey(EtapaHistoriaUsuario, related_name='historias')
+    tipo = models.ForeignKey(TipoHistoriaUsusario, related_name='historias')
     versionPrevia = models.ForeignKey('HistoriaUsuario', related_name='versionPrevia', blank=True, null=True)
     up = models.IntegerField(blank=False)
     bv = models.IntegerField(blank=False)
     usuarioAsignado = models.ForeignKey('Usuario', related_name='usuarioAsignado', blank=True, null=True)
     proyecto = models.ForeignKey(Proyecto, related_name='backlog')
+    archivo = models.ManyToManyField(ArchivoAnexo, blank=True)
 
     def __str__(self):
         return self.nombre
 
 
 class Comentario(models.Model):
+    """
+    Comentario en una historia de usuario.
+    """
     contenido = models.TextField(blank=True, null=True)
     usuario = models.ForeignKey('Usuario', related_name='comentarios')
     historiaUsuario = models.ForeignKey(HistoriaUsuario, related_name='comentarios')
