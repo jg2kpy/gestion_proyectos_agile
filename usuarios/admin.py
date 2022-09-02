7a58652867e6bdddecfe455635b9c52ecf41a56c
Guillermo <guigapamplona@gmail.com>
2022-09-02 15:09:25 -0400

    Visualización de roles globales en menú admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from usuarios.models import RolSistema
 
 # Register your models here.
+
+
+
+# Temporal despues probablemente deba borrar
+"""
+Muestra Preview de los campos en admin
+"""
+
+class RolSistemaAdmin(admin.ModelAdmin):
+   list_display = ('nombre', 'descripcion') 
+
+"""
+Registra el modelo en la vista de admin
+"""
+admin.site.register(RolSistema, RolSistemaAdmin)
