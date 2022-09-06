c6934230642ba441d033673ae9de23883d7fc3ec
Manuel Rene Pauls Toews <contact@renepauls.com>
2022-09-05 21:27:17 -0400

    Traer variables de tiempo de ejecucion recién cuando llega el request

--- proyectos/forms.py
+++ proyectos/forms.py
@@ -1,12 +1,11 @@
 from django import forms
 from usuarios.models import Usuario
 from .models import Proyecto
-#Traemos los roles y los permisos de los proyectos
+# Traemos los roles y los permisos de los proyectos
 from usuarios.models import RolProyecto, PermisoProyecto
 
 
-
-#Estados de proyecto
+# Estados de proyecto
 ESTADOS_PROYECTO = (
     ('Planificacion', 'Planificacion'),
     ('Ejecucion', 'Ejecucion'),
@@ -15,37 +14,28 @@ ESTADOS_PROYECTO = (
     ('En espera', 'En espera'),
 )
 
-#traemos los usuarios del sistema
-USUARIOS = (
-    (usuario.id, usuario.email) for usuario in Usuario.objects.all()
-)
-
-#Traemos los Permisos de los proyectos
-PERMISOS = (
-    (permiso.id, permiso.descripcion) for permiso in PermisoProyecto.objects.all()
-)
-
-PROYECTOS = (
-    (proyecto.id, proyecto.nombre) for proyecto in Proyecto.objects.all()
-)
 
 class ProyectoForm(forms.Form):
     nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     descripcion = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
     fecha_inicio = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control'}))
     fecha_fin = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control'}))
-    scrum_master = forms.ChoiceField(choices=USUARIOS, widget=forms.Select(attrs={'class': 'form-control'}))
+    scrum_master = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}))
 
 
 class ProyectoCancelForm(forms.Form):
     nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
 
-#Crear un Rol de Proyecto
+# Crear un Rol de Proyecto
+
+
 class RolProyectoForm(forms.Form):
     nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     descripcion = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
-    permisos = forms.ModelMultipleChoiceField(queryset=PermisoProyecto.objects.all(), widget=forms.CheckboxSelectMultiple())
+    permisos = forms.ModelMultipleChoiceField(queryset=None, widget=forms.CheckboxSelectMultiple())
 
-#Form para importar roles de otros proyectos
+# Form para importar roles de otros proyectos
+
+
 class ImportarRolProyectoForm(forms.Form):
-    proyecto = forms.ChoiceField(choices= PROYECTOS,widget=forms.Select(attrs={'class': 'form-control'}))
+    proyecto = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}))
--- proyectos/views.py
+++ proyectos/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import Proyecto
 from .forms import ProyectoForm, ProyectoCancelForm, RolProyectoForm, ImportarRolProyectoForm
-from usuarios.models import Usuario , RolProyecto, PermisoProyecto
+from usuarios.models import Usuario, RolProyecto, PermisoProyecto
 
-#Estados de Proyecto
+# Estados de Proyecto
 ESTADOS_PROYECTO = (
     ('Planificacion', 'Planificacion'),
     ('Ejecucion', 'Ejecucion'),
@@ -13,16 +13,13 @@ ESTADOS_PROYECTO = (
 )
 
 
-
-#Creamos una vista para ver los proyectos
+# Creamos una vista para ver los proyectos
 def proyectos(request):
-    return render(request, 'proyectos/base.html', {'proyectos': Proyecto.objects.all() ,'usuario' : request.user})
-
+    return render(request, 'proyectos/base.html', {'proyectos': Proyecto.objects.all(), 'usuario': request.user})
 
 
 # Recibimos una peticion POST para crear un proyecto
 def crear_proyecto(request):
-
     if request.method == 'POST':
         form = ProyectoForm(request.POST)
         if form.is_valid():
@@ -32,18 +29,18 @@ def crear_proyecto(request):
             proyecto.descripcion = form.cleaned_data['descripcion']
             proyecto.fecha_inicio = form.cleaned_data['fecha_inicio']
             proyecto.fecha_fin = form.cleaned_data['fecha_fin']
-            proyecto.estado = ESTADOS_PROYECTO.__getitem__(0)[0] #Automaticamente el estado se queda en planificado
+            proyecto.estado = ESTADOS_PROYECTO.__getitem__(0)[0]  # Automaticamente el estado se queda en planificado
             id_scrum_master = form.cleaned_data['scrum_master']
             scrum_master = Usuario.objects.get(id=id_scrum_master)
             proyecto.scrumMaster = scrum_master
             proyecto.save()
 
             scrum_master.equipo.add(proyecto)
-            
-            #Traemos el ID del proyecto recien creado
-            #Traemos todos los roles que tenga null como proyecto_id
-            roles = RolProyecto.objects.filter(proyecto__isnull = True)
-            #Generamos una copia de este rol y el asignamos el id del proyecto
+
+            # Traemos el ID del proyecto recien creado
+            # Traemos todos los roles que tenga null como proyecto_id
+            roles = RolProyecto.objects.filter(proyecto__isnull=True)
+            # Generamos una copia de este rol y el asignamos el id del proyecto
             for rol in roles:
                 # Creamos el rol
                 rol_nuevo = RolProyecto()
@@ -57,49 +54,57 @@ def crear_proyecto(request):
 
                 # Recorremos los permisos y los asignamos al rol
                 for permiso in permisos:
-                    #Agregamos el rol al permiso
+                    # Agregamos el rol al permiso
                     permiso.rol.add(rol_nuevo)
                     permiso.save()
-            
-            scrum_master.roles_proyecto.add(RolProyecto.objects.get(nombre = "Scrum Master", proyecto=proyecto))
+
+            scrum_master.roles_proyecto.add(RolProyecto.objects.get(nombre="Scrum Master", proyecto=proyecto))
             scrum_master.save()
 
             return render(request, 'proyectos/base.html', {'proyectos': Proyecto.objects.all()})
     else:
         form = ProyectoForm()
+        form.scrum_master.choices = [(proyecto.id, proyecto.nombre) for proyecto in Proyecto.objects.all()]
         formRol = RolProyectoForm()
-    return render(request, 'proyectos/crear_proyecto.html', {'form': form , 'form_rol': formRol})
+        formRol.permisos = [(permiso.id, permiso.descripcion) for permiso in PermisoProyecto.objects.all()]
+    return render(request, 'proyectos/crear_proyecto.html', {'form': form, 'form_rol': formRol})
 
 # Recibimos una peticion POST para cancelar un proyecto
+
+
 def cancelar_proyecto(request, id_proyecto):
     # Verificamos que el usuario tenga permisos rol de moderador o es el scrum master del proyecto
     if request.method == 'POST':
         form = ProyectoCancelForm(request.POST)
         if form.is_valid():
             # Cancelamos el proyecto
-            #verificamos que el nombre del proyecto sea correcto
+            # verificamos que el nombre del proyecto sea correcto
             if form.cleaned_data['nombre'] == Proyecto.objects.get(id=id_proyecto).nombre:
                 proyecto = Proyecto.objects.get(id=id_proyecto)
                 proyecto.estado = 'Cancelado'
                 proyecto.save()
                 return render(request, 'proyectos/base.html', {'proyectos': Proyecto.objects.all()})
-            else: 
+            else:
                 print('El nombre del proyecto no es correcto')
                 return render(request, 'proyectos/base.html', {'proyectos': Proyecto.objects.all()})
     else:
         form = ProyectoCancelForm()
-    
-    #Si el proyecto ya esta cancelado no se puede cancelar de nuevo
+
+    # Si el proyecto ya esta cancelado no se puede cancelar de nuevo
     if Proyecto.objects.get(id=id_proyecto).estado == ESTADOS_PROYECTO.__getitem__(3)[0]:
         return render(request, 'proyectos/base.html', {'proyectos': Proyecto.objects.all()})
 
     return render(request, 'proyectos/cancelar_proyecto.html', {'form': form})
 
 # Creamos una vista para ver los roles de proyectos
+
+
 def roles_proyecto(request):
-    return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': RolProyecto.objects.all() ,'usuario' : request.user})
+    return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': RolProyecto.objects.all(), 'usuario': request.user})
 
 # Creamos un rol en un proyecto
+
+
 def crear_rol_proyecto(request):
     if request.method == 'POST':
         form = RolProyectoForm(request.POST)
@@ -119,17 +124,19 @@ def crear_rol_proyecto(request):
             # Recorremos los permisos y los asignamos al rol
             for permiso in permisos:
                 if permiso.nombre in permisos_seleccionados.values_list('nombre', flat=True):
-                    #Agregamos el rol al permiso
+                    # Agregamos el rol al permiso
                     permiso.rol.add(rol)
                     permiso.save()
-                    
 
-            return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': RolProyecto.objects.all() ,'usuario' : request.user})
+            return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': RolProyecto.objects.all(), 'usuario': request.user})
     else:
         form = RolProyectoForm()
+        form.permisos = [(permiso.id, permiso.descripcion) for permiso in PermisoProyecto.objects.all()]
     return render(request, 'proyectos/roles_proyecto/crear_rol_proyecto.html', {'form': form})
 
 # Ver la informacion de un rol de un proyecto en especifico
+
+
 def ver_rol_proyecto(request, id_rol_proyecto):
     rol = RolProyecto.objects.get(id=id_rol_proyecto)
 
@@ -139,6 +146,8 @@ def ver_rol_proyecto(request, id_rol_proyecto):
     return render(request, 'proyectos/roles_proyecto/ver_rol_proyecto.html', {'rol': rol, 'permisos': permisos})
 
 # Modificar un rol de un proyecto
+
+
 def modificar_rol_proyecto(request, id_rol_proyecto):
     rol = RolProyecto.objects.get(id=id_rol_proyecto)
 
@@ -162,35 +171,42 @@ def modificar_rol_proyecto(request, id_rol_proyecto):
             # Recorremos los permisos y los asignamos al rol
             for permiso in permisos:
                 if permiso.nombre in permisos_seleccionados.values_list('nombre', flat=True):
-                    #Agregamos el rol al permiso
+                    # Agregamos el rol al permiso
                     permiso.rol.add(rol)
                     permiso.save()
                 else:
-                    #Eliminamos el rol del permiso
+                    # Eliminamos el rol del permiso
                     permiso.rol.remove(rol)
                     permiso.save()
 
-            return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': RolProyecto.objects.all() ,'usuario' : request.user})
+            return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': RolProyecto.objects.all(), 'usuario': request.user})
     else:
         form = RolProyectoForm(initial={'nombre': rol.nombre, 'descripcion': rol.descripcion})
+        form.permisos = [(permiso.id, permiso.descripcion) for permiso in PermisoProyecto.objects.all()]
     return render(request, 'proyectos/roles_proyecto/modificar_rol_proyecto.html', {'form': form, 'rol': rol, 'permisos': permisos})
 
 # Eliminar un rol de un proyecto
+
+
 def eliminar_rol_proyecto(request, id_rol_proyecto):
     if request.method == 'POST':
         rol = RolProyecto.objects.get(id=id_rol_proyecto)
         rol.delete()
-        return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': RolProyecto.objects.all() ,'usuario' : request.user})
+        return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': RolProyecto.objects.all(), 'usuario': request.user})
     return render(request, 'proyectos/roles_proyecto/eliminar_rol_proyecto.html', {'rol_proyecto': RolProyecto.objects.get(id=id_rol_proyecto)})
 
 # Ver roles asignados a un proyecto
+
+
 def ver_roles_asignados(request, id_proyecto):
     # Traemos los roles que tengan el id del proyecto
     roles = RolProyecto.objects.filter(proyecto=id_proyecto)
-    
+
     return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': roles, 'id_proyecto': id_proyecto})
 
 # Creamos un rol en un proyecto a un proyecto especifico
+
+
 def crear_rol_a_proyecto(request, id_proyecto):
     if request.method == 'POST':
         form = RolProyectoForm(request.POST)
@@ -211,17 +227,19 @@ def crear_rol_a_proyecto(request, id_proyecto):
             # Recorremos los permisos y los asignamos al rol
             for permiso in permisos:
                 if permiso.nombre in permisos_seleccionados.values_list('nombre', flat=True):
-                    #Agregamos el rol al permiso
+                    # Agregamos el rol al permiso
                     permiso.rol.add(rol)
                     permiso.save()
-                    
 
             return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': RolProyecto.objects.filter(proyecto=id_proyecto)})
     else:
         form = RolProyectoForm()
+        form.permisos = [(permiso.id, permiso.descripcion) for permiso in PermisoProyecto.objects.all()]
     return render(request, 'proyectos/roles_proyecto/crear_rol_proyecto.html', {'form': form, 'id_proyecto': id_proyecto})
 
-#Importar Rol de otros proyectos
+# Importar Rol de otros proyectos
+
+
 def importar_rol(request, id_proyecto):
     if request.method == 'POST':
         form = ImportarRolProyectoForm(request.POST)
@@ -246,14 +264,13 @@ def importar_rol(request, id_proyecto):
 
                 # Recorremos los permisos y los asignamos al rol
                 for permiso in permisos:
-                    #Agregamos el rol al permiso
+                    # Agregamos el rol al permiso
                     permiso.rol.add(rol_nuevo)
                     permiso.save()
-
 
             return render(request, 'proyectos/roles_proyecto/roles_proyecto.html', {'roles_proyecto': RolProyecto.objects.filter(proyecto=id_proyecto)})
 
     else:
         form = ImportarRolProyectoForm()
+        form.choices = [(proyecto.id, proyecto.nombre) for proyecto in Proyecto.objects.all()]
     return render(request, 'proyectos/roles_proyecto/importar_rol.html', {'form': form, 'id_proyecto': id_proyecto})
-
