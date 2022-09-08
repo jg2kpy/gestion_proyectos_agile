803f2e0416a6d1337e297247568886e6c9d70eac
Manuel Rene Pauls Toews <contact@renepauls.com>
2022-09-07 22:06:43 -0400

    Se puede eliminar tipos de historia de usuario

--- historias_usuario/views.py
+++ historias_usuario/views.py
@@ -23,7 +23,7 @@ def tiposHistoriaUsario(request, proyecto_id):
     if not tiene_permiso_en_proyecto(request.user, "pro_crearTipoUS", proyecto):
         return HttpResponseRedirect("/", status=422)
 
-    return render(request, 'tipos-us/base.html', {'tipo-us': TipoHistoriaUsusario.objects.all(), 'proyecto': proyecto})
+    return render(request, 'tipos-us/base.html', {'tipos': TipoHistoriaUsusario.objects.all(), 'proyecto': proyecto})
 
 
 @never_cache
@@ -71,52 +71,76 @@ def crear_tipoHistoriaUsuario(request, proyecto_id):
     return render(request, 'tipos-us/crear_tipo.html', {'historiaformset': formset, 'form': form, 'proyecto': proyecto}, status=status)
 
 
-# @never_cache
-# def eliminar_tipoHistoriaUsuario(request, id_proyecto):
-#     if not request.user.is_authenticated:
-#         return HttpResponseRedirect("/", status=401)
+@never_cache
+def borrar_tipoHistoriaUsuario(request, proyecto_id, tipo_id):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect("/", status=401)
 
-#     # TODO: Validar que el usuario tiene permiso en proyecto
+    try:
+        proyecto = Proyecto.objects.get(id=proyecto_id)
+    except Proyecto.DoesNotExist:
+        return render(request, '404.html', {'info_adicional': "No se encontró este proyecto."}, status=404)
+    try:
+        tipo = TipoHistoriaUsusario.objects.get(id=tipo_id)
+    except Proyecto.DoesNotExist:
+        return render(request, '404.html', {'info_adicional': "No se encontró este tipo de historia de usuario."}, status=404)
 
-#     try:
-#         proyecto = Proyecto.objects.get(id=id_proyecto)
-#     except Proyecto.DoesNotExist:
-#         return render(request, '404.html', {'info_adicional': "No se encontró este proyecto."}, status=404)
+    if not tiene_permiso_en_proyecto(request.user, "pro_eliminarTipoUS", proyecto):
+        return HttpResponseRedirect("/", status=422)
 
-#     status = 200
-#     if request.method == 'POST':
-#         form = TipoHistoriaUsuarioEliminarForm(request.POST)
-#         if form.is_valid():
-#             return render(request, 'tipos-us/base.html', {'tipo-us': TipoHistoriaUsusario.objects.all(), 'proyecto': proyecto}, status=status)
-#         else:
-#             status = 422
-#     else:
-#         form = TipoHistoriaUsuarioEliminarForm()
+    status = 200
+    if request.method == 'POST':
+        try:
+            tipo = TipoHistoriaUsusario.objects.get(id=tipo_id)
+            tipo.delete()
+        except TipoHistoriaUsusario.DoesNotExist:
+            pass
+        status = 200
+        return HttpResponseRedirect(f"/tipo-historia-usuario/{proyecto.id}/")
 
-#     return render(request, 'tipos-us/eliminar_us.html', {'form': form, 'proyecto': proyecto}, status=status)
+    return render(request, 'tipos-us/eliminar_tipo.html', {'tipo': tipo, 'proyecto': proyecto, }, status=status)
 
 
-# @never_cache
-# def editar_tipoHistoriaUsuario(request, id_proyecto, id_tipo_us):
-#     if not request.user.is_authenticated:
-#         return HttpResponseRedirect("/", status=401)
+@never_cache
+def editar_tipoHistoriaUsuario(request, proyecto_id, tipo_id):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect("/", status=401)
 
-#     try:
-#         proyecto = Proyecto.objects.get(id=id_proyecto)
-#     except Proyecto.DoesNotExist:
-#         return render(request, '404.html', {'info_adicional': "No se encontró este proyecto."}, status=404)
-#     try:
-#         tipo_us = tipo_us
-#     except TipoHistoriaUsusario.DoesNotExist:
-#         return render(request, '404.html', {'info_adicional': "No se encontró este tipo de historia de usuario."}, status=404)
+    try:
+        proyecto = Proyecto.objects.get(id=proyecto_id)
+    except Proyecto.DoesNotExist:
+        return render(request, '404.html', {'info_adicional': "No se encontró este proyecto."}, status=404)
 
-#     status = 200
-#     if request.method == 'POST':
-#         form = TipoHistoriaUsusarioForm(request.POST, instance=tipo_us)
-#         if form.is_valid():
-#             return render(request, 'tipos-us/base.html', {'tipo-us': TipoHistoriaUsusario.objects.all(), 'proyecto': proyecto}, status=status)
-#         else:
-#             status = 422
-#     else:
-#         form = TipoHistoriaUsusarioForm(tipo_us)
-#     return render(request, 'tipo-us/editar.html', {'form': form, 'proyecto': proyecto}, status=status)
+    if not tiene_permiso_en_proyecto(request.user, "pro_crearTipoUS", proyecto):
+        return HttpResponseRedirect("/", status=422)
+
+    status = 200
+    formset_factory = inlineformset_factory(TipoHistoriaUsusario, EtapaHistoriaUsuario, form=EtapaHistoriaUsuarioForm, extra=1, can_delete=False)
+    if request.method == 'POST':
+        form = TipoHistoriaUsuarioForm(request.POST)
+        formset = formset_factory(request.POST, instance=form.instance)
+        if form.is_valid() and formset.is_valid():
+            tipo = form.save(commit=False)
+            tipo.proyecto = proyecto
+            if tipo.nombre in [t.nombre for t in proyecto.tiposHistoriaUsuario.all()]:
+                form.add_error('nombre', "Ya existe un tipo de historia de usuario con este nombre.")
+                status = 422
+            elif '' in [e.instance.nombre for e in formset]:
+                form.add_error(None, "No puede haber etapas sin nombre.")
+                status = 422
+            else:
+                tipo.save()
+                for i, etapa_form in enumerate(formset):
+                    etapa = etapa_form.save(commit=False)
+                    etapa.orden = i
+                    etapa.save()
+                status = 200
+                return HttpResponseRedirect(f"/tipo-historia-usuario/{proyecto.id}/")
+        else:
+            form.add_error(None, "Hay errores en el formulario.")
+            status = 422
+        pass
+    else:
+        form = TipoHistoriaUsuarioForm()
+        formset = formset_factory()
+    return render(request, 'tipo-us/editar.html', {'form': form, 'proyecto': proyecto}, status=status)
