77c9bc9576d1b5eaf5a0ead5262f8a338e5de7df
Manuel Rene Pauls Toews <contact@renepauls.com>
2022-09-07 21:31:17 -0400

    Se pueden crear tipos de historias de usuario

--- historias_usuario/urls.py
+++ historias_usuario/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import crear_tipoHistoriaUsuario
+from .views import crear_tipoHistoriaUsuario, tiposHistoriaUsario
 
 urlpatterns = [
-    path('crear/<int:proyecto_id>/', crear_tipoHistoriaUsuario, name='TiposHistoriaUsuario'),
+    path('<int:proyecto_id>/', tiposHistoriaUsario, name='TiposHistoriaUsuario'),
+    path('crear/<int:proyecto_id>/', crear_tipoHistoriaUsuario, name='Crear Historia Usuario'),
 ]
--- historias_usuario/views.py
+++ historias_usuario/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.decorators.cache import never_cache
 from django.forms import inlineformset_factory
+
+from gestion_proyectos_agile.templatetags.tiene_rol_en import tiene_permiso_en_proyecto
 from .forms import EtapaHistoriaUsuarioForm, TipoHistoriaUsuarioForm
 
 from proyectos.models import Proyecto
@@ -9,16 +11,18 @@ from .models import EtapaHistoriaUsuario, TipoHistoriaUsusario
 
 
 @never_cache
-def tiposHistoriaUsario(request, id_proyecto):
+def tiposHistoriaUsario(request, proyecto_id):
     if not request.user.is_authenticated:
         return HttpResponseRedirect("/", status=401)
 
     try:
-        proyecto = Proyecto.objects.get(id=id_proyecto)
+        proyecto = Proyecto.objects.get(id=proyecto_id)
     except Proyecto.DoesNotExist:
         return render(request, '404.html', {'info_adicional': "No se encontró este proyecto."}, status=404)
 
-    # TODO: Validar que el usuario tiene permiso en proyecto
+    if not tiene_permiso_en_proyecto(request.user, "pro_crearTipoUS", proyecto):
+        return HttpResponseRedirect("/", status=422)
+
     return render(request, 'tipos-us/base.html', {'tipo-us': TipoHistoriaUsusario.objects.all(), 'proyecto': proyecto})
 
 
@@ -27,12 +31,13 @@ def crear_tipoHistoriaUsuario(request, proyecto_id):
     if not request.user.is_authenticated:
         return HttpResponseRedirect("/", status=401)
 
-    # TODO: Validar que el usuario tiene permiso en proyecto
-
     try:
         proyecto = Proyecto.objects.get(id=proyecto_id)
     except Proyecto.DoesNotExist:
         return render(request, '404.html', {'info_adicional': "No se encontró este proyecto."}, status=404)
+
+    if not tiene_permiso_en_proyecto(request.user, "pro_crearTipoUS", proyecto):
+        return HttpResponseRedirect("/", status=422)
 
     status = 200
     formset_factory = inlineformset_factory(TipoHistoriaUsusario, EtapaHistoriaUsuario, form=EtapaHistoriaUsuarioForm, extra=1, can_delete=False)
@@ -55,7 +60,7 @@ def crear_tipoHistoriaUsuario(request, proyecto_id):
                     etapa.orden = i
                     etapa.save()
                 status = 200
-                return HttpResponseRedirect(f"/proyectos/{proyecto.id}/tipos-historia-usuario/")
+                return HttpResponseRedirect(f"/tipo-historia-usuario/{proyecto.id}/")
         else:
             form.add_error(None, "Hay errores en el formulario.")
             status = 422
